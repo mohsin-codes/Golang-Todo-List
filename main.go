70ec1d7ee104e69e5761e691f62e94184aabe11c
Mohsin <mohsink9899@gmail.com>
2021-09-27 11:52:24 +0530

    Created a todomodel struct(bson - to interact with database) and todo struct(json - to interact with frontend)

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/thedevsaddam/renderer"
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"gppkg.in/mgo.v2/bson"
 )
 
 var rnd *renderer.Render
@@ -15,6 +28,18 @@ const (
 	port           string = ":9000"
 )
 
-func main() {
+type(
+	todoModel struct{
+		ID 			bson.ObjectId `bson:"_id,omitempty"`
+		Title		string `bson:"title"`
+		Completed	bool `bson:"completed"`
+		CreatedAt	time.Time `bson: "createAt"`
+	}
 
-}
+	todo struct {
+		ID 			string `json:"id"`
+		Title		string `json:"title"`
+		Completed 	bool `json:"completed"`
+		CreatedAt	time.Time `json:"created_At"`
+	}
+)
